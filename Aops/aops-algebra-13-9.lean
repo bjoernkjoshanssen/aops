a66028cf6c19a0b304b1731d22343286c16b2201
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2026-03-29 09:00:33 -1000

    fix AOPS problem

--- Aops/aops-algebra-13-9.lean
+++ Aops/aops-algebra-13-9.lean
@@ -3,48 +3,51 @@ import Mathlib.RingTheory.Int.Basic
 import Mathlib.Data.Finset.Basic
 import Mathlib.Algebra.QuadraticDiscriminant
 
-/- Art of Problem Solving
+open Complex
+
+/-- Art of Problem Solving
 Algebra
 Problem 13.9
 
 z^2 + 6iz = -7
 -/
-
-example (z : ℂ) : z^2 + 6 * Complex.I * z = -7 ↔ z = Complex.I ∨ z = - 7 * Complex.I := by
+lemma algebra_13_9 (z : ℂ) : z^2 + 6 * I * z = -7 ↔ z = I ∨ z = - 7 * I := by
   constructor
   . intro h
-    have : 1 * z * z + 6 * Complex.I * z = -7 := by
+    have : 1 * z * z + 6 * I * z = -7 := by
       rw [← h]
       simp
       ring_nf
-    have : 1 * z * z + 6 * Complex.I * z + 7 = 0 := by
+    have : 1 * z * z + 6 * I * z + 7 = 0 := by
       rw [this]
       simp
     have Q := @quadratic_eq_zero_iff
-      ℂ _ _ 1 (6 * Complex.I) 7
-      (Ne.symm (zero_ne_one' ℂ)) (8 * Complex.I)
+      ℂ _ _ 1 (6 * I) 7
+      (Ne.symm (zero_ne_one' ℂ)) (8 * I)
       (by
         unfold discrim
         simp
         ring_nf
-        rw [Complex.I_sq]
+        rw [I_sq]
         simp
         ring_nf
       ) z
+    rw [mul_assoc] at this
     rw [Q] at this
     simp at this
     cases this with
-    |inl h => left;rw [h];ring_nf
+    |inl h => left;rw [h];ring_nf;simp;exact Nat.cast_one
     |inr h => right;rw [h];ring_nf
   . intro h
     cases h with
     |inl hl =>
       subst hl
       ring_nf
-      rw [Complex.I_sq]
+      rw [I_sq]
       simp
     |inr hr =>
       subst hr
       ring_nf
-      rw [Complex.I_sq]
+      rw [I_sq]
       simp
+      rfl
